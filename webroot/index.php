ca73d858307f05b228d0d2f8ba3fc178f037c2fe
the-source <mtkam@outlook.com>
2020-09-05 01:48:06 +0200

    + base webroot structure + index.php checks     * php     * database     * css     * js

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Samplesite</title>
+    <link rel="stylesheet" href="styles/base.css">
+</head>
+<body>
+
+<h1>Samplesite</h1>
+
+<p>PHP: <?php echo 'hello world' ?></p>
+<p>Database: <?php
 $db = new PDO('mysql:host=mysql;dbname=sample_database;charset=utf8mb4', 'root', $_ENV['MYSQL_ROOT_PASSWORD']);
-echo ($db->query('SELECT `value` FROM `hello_table`'))->fetchAll(PDO::FETCH_OBJ)[0]->value;
+echo ($db->query('SELECT `value` FROM `hello_table`'))->fetchAll(PDO::FETCH_OBJ)[0]->value;?></p>
+<p class="css">CSS: </p>
+<p id="js">JavaScript:</p>
+</body>
+<script src="js/base.js"></script>
+</html>
---
+++ webroot/js/base.js
@@ -0,0 +1,2 @@
+let elem = document.getElementById('js');
+elem.innerText = elem.innerText + " hello world";
